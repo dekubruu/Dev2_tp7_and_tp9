29f282fe7bf20f3515f57826304b745cc1f5aef4
dekubruu <aktemirov2001@gmail.com>
2024-12-06 19:48:09 +0100

    - test unit supplémentaire dans test_unit_and_properties
    - test unit supplémentaire dans test_as_mixed_numbers
    
    -test unit supplémentaire dans test_operations

--- test_unit.py
+++ test_unit.py
@@ -6,25 +6,49 @@ class TestFraction(unittest.TestCase):
 
     def test_init_and_properties(self):
         """Test de l'initialisation et des propriétés."""
+
+        """ Fraction positive"""
         f1 = Fraction(3, 4)
         self.assertEqual(f1.numerator, 3)
         self.assertEqual(f1.denominator, 4)
 
+        f5 = Fraction(5, 6)
+        self.assertEqual(f5.numerator, 5)
+        self.assertEqual(f5.denominator, 6)
+
+        """ Fraction negative"""
         f2 = Fraction(-5, 10)
         self.assertEqual(f2.numerator, -1)
         self.assertEqual(f2.denominator, 2)
 
+        f6 = Fraction(-6, 7)
+        self.assertEqual(f6.numerator, -6)
+        self.assertEqual(f6.denominator, 7)
+
+        """ Fraction zero"""
         f3 = Fraction(0, 7)
         self.assertEqual(f3.numerator, 0)
         self.assertEqual(f3.denominator, 1)
+        """ Fraction un"""
+        f4 = Fraction(7, 7)
+        self.assertEqual(f4.numerator,1)
+        self.assertEqual(f4.denominator,1)
 
         with self.assertRaises(ValueError):
             Fraction(1, 0)  # Test d'une exception pour un dénominateur nul
 
     def test_as_mixed_number(self):
         """Test de la conversion en nombre mixte."""
+
+        """Fraction positive"""
         self.assertEqual(Fraction(7, 2).as_mixed_number(), "3 1/2")
+        self.assertEqual(Fraction(6, 2).as_mixed_number(), "3")
+
+        """Fraction negative"""
         self.assertEqual(Fraction(-7, 2).as_mixed_number(), "-3 1/2")
+        self.assertEqual(Fraction(-8, 3).as_mixed_number(), "-2 2/3")
+
+        """Test supplémentaire"""
         self.assertEqual(Fraction(4, 2).as_mixed_number(), "2")
         self.assertEqual(Fraction(1, 2).as_mixed_number(), "1/2")
 
@@ -34,24 +58,41 @@ class TestFraction(unittest.TestCase):
         self.assertFalse(Fraction(3, 2).is_proper())
         self.assertTrue(Fraction(-1, 2).is_proper())
         self.assertFalse(Fraction(-3, 2).is_proper())
+        self.assertTrue(Fraction(10, 10).is_proper())
 
     def test_operations(self):
         """Test des opérations arithmétiques."""
-        # Addition
+        # Addition Fraction Positive
         self.assertEqual(Fraction(1, 2) + Fraction(1, 2), Fraction(1, 1))
         self.assertEqual(Fraction(3, 4) + Fraction(-1, 4), Fraction(1, 2))
 
-        # Soustraction
+        # Addition Fraction Négative
+        self.assertEqual(Fraction(-1, 2) + Fraction(-1, 2), Fraction(-1, 1))
+        self.assertEqual(Fraction(-3, 4) + Fraction(-1, 4), Fraction(1, 2))
+
+        # Soustraction fraction positive
         self.assertEqual(Fraction(3, 4) - Fraction(1, 4), Fraction(1, 2))
         self.assertEqual(Fraction(1, 2) - Fraction(3, 4), Fraction(-1, 4))
 
-        # Multiplication
-        self.assertEqual(Fraction(2, 3) * Fraction(3, 4), Fraction(1, 2))
-        self.assertEqual(Fraction(-2, 3) * Fraction(3, 4), Fraction(-1, 2))
+        # Soustraction fraction negative
+        self.assertEqual(Fraction(-5, 1) - Fraction(-1, 1), Fraction(-4, 1))
+        self.assertEqual(Fraction(-3, 4) - Fraction(-1, 3), Fraction(-5, 12))
 
-        # Division
+        # Multiplication positive
+        self.assertEqual(Fraction(2, 3) * Fraction(3, 4), Fraction(1, 2))
+        self.assertEqual(Fraction(5, 3) * Fraction(5, 4), Fraction(5, 3))
+
+        # Multiplication negative
+        self.assertEqual(Fraction(-2, 3) * Fraction(-3, 4), Fraction(1, 2))
+        self.assertEqual(Fraction(-5, 3) * Fraction(-5, 4), Fraction(5, 3))
+
+        # Division positive
         self.assertEqual(Fraction(1, 2) / Fraction(1, 4), Fraction(2, 1))
-        self.assertEqual(Fraction(-1, 2) / Fraction(1, 4), Fraction(-2, 1))
+        self.assertEqual(Fraction(3, 2) / Fraction(5, 4), Fraction(6, 5))
+
+        # Division negative
+        self.assertEqual(Fraction(-1, 2) / Fraction(-1, 4), Fraction(2, 1))
+        self.assertEqual(Fraction(-3, 2) / Fraction(-5, 4), Fraction(6, 5))
 
         with self.assertRaises(ZeroDivisionError):
             Fraction(1, 2) / Fraction(0, 1)
